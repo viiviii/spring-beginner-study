f969a7b56a0cf56ce0d6536fa95da8e74ac45c92
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 00:44:25 +0900

    Implement MemberService

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,26 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 룰: 같은 이름의 중복 회원 X
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
