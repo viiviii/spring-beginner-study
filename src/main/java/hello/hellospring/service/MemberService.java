db50db14941b6251a55977cb47d2057d3215caca
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 00:49:02 +0900

    Simplify join()

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -15,10 +15,10 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 룰: 같은 이름의 중복 회원 X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다");
-        });
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
         memberRepository.save(member);
         return member.getId();
     }
