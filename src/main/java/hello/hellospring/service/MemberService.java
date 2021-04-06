f743b6d405c8de9cf9c95e7499cf54e25cb87485
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 00:59:08 +0900

    Extract validateDuplicateMember()

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,13 +14,16 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        // 룰: 같은 이름의 중복 회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다");
                 });
-        memberRepository.save(member);
-        return member.getId();
     }
 
 }
