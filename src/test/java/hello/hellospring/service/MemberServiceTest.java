8c1599373f4ebfe83702fd770d5f770fcdfae138
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 23:51:29 +0900

    Verify exception message

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -38,7 +38,9 @@ class MemberServiceTest {
         memberService.join(member1);
 
         //Then
-        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
     }
 
     @Test
