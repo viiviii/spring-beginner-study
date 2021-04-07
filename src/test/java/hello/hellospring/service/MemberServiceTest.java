7a52706e49bf4fa799b1de52c931a90c1b8f7cc5
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 23:16:33 +0900

    Add test 회원가입()

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
@@ -9,7 +12,17 @@ class MemberServiceTest {
     MemberService memberService = new MemberService();
 
     @Test
-    void join() {
+    void 회원가입() {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+
+        //When
+        Long saveId = memberService.join(member);
+
+        //Then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
