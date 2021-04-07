dd2099aa6159e77dc45b6248e11f03962ac2d170
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 22:12:46 +0900

    Implement MemberServiceTest with stub

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
