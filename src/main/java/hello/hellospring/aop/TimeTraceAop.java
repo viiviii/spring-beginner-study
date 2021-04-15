c168da86a9e2d824999e3cd34e364e62e1b55f46
viiviii <viiiix.viiviii@gmail.com>
2021-04-15 13:47:31 +0900

    Fix BeanCurrentlyInCreationException
    https://www.inflearn.com/questions/48156

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -3,12 +3,11 @@ package hello.hellospring.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
 @Aspect
 public class TimeTraceAop {
 
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring..*(..)) && !target(hello.hellospring.SpringConfig)")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString());
